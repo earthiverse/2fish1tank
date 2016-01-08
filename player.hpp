c5cf6848c468285fad6e4ef96b3e58ac437d8140
Kent Rasmussen <krasmuss@ualberta.ca>
2016-01-08 03:59:14 +0000

    Tnkas

--- player.hpp
+++ player.hpp
@@ -7,4 +7,5 @@ class Player {
 private:
   string name;
   float score;
+  
 };
--- tank.hpp
+++ tank.hpp
@@ -7,8 +7,8 @@ class Tank {
   friend class State;
 
 public:
-  Tank(string _id, bool _alive, double _hp);
-	
+  //Tank(string _id, bool _alive, double _hp);
+	Tank();
 	void move_tank(float x, float y);
 	void rotate_tank(float rads);
 	void rotate_cannon(float rads);
@@ -28,21 +28,28 @@ private:
   double tracks;
   double turret;
 };
-
+/*
 Tank::Tank(string _id, bool _alive, double _hp) : id(_id), alive(_alive), health(_hp) {
 }
-
+*/
+Tank::Tank() {
+  alive = false;
+}
 
 void Tank::move_tank(float x, float y) {
+  Command::Instance().move_tank(id, x, y);
 }
 
 void Tank::rotate_tank(float rads){
+  Command::Instance().rotate_tank(id, rads);
 }
 
 void Tank::rotate_cannon(float rads){
+  Command::Instance().rotate_cannon(id, rads);
 }
 
 void Tank::fire_cannon(){
+  Command::Instance().fire(id);
 }
 
 
